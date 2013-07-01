0e84ef04787e622133e566c52dfce1fd9fbcf521
Jen E <jeneisenberg5@gmail.com>
2013-06-30 22:25:55 -0400

    all working now

--- app.rb
+++ app.rb
@@ -18,5 +18,26 @@ module Citibike
       erb :home
     end
 
+    get '/form' do
+      erb :form
+    end
+
+    post '/form' do
+      @results = params
+      erb :view
+    end
+
+    post '/map' do
+      @start = params["start"].split(",").collect do |coords|
+        coords.to_f / 1000000
+      end
+      
+      @end = params["end"].split(",").collect do |coords|
+        coords.to_f / 1000000
+      end
+      
+      erb :map
+    end
+
   end
 end
